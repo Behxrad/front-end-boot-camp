2b47d7f119c0b83fc695fb30c417795fde198cf5
Behxrad <behxrad@yahoo.com>
2025-07-20 22:55:01 +0330

    Opinion board updated.

--- React/18. Form Actions/2. Opinion Board (Advanced)/src/components/NewOpinion.jsx
+++ React/18. Form Actions/2. Opinion Board (Advanced)/src/components/NewOpinion.jsx
@@ -1,28 +1,81 @@
+import { useActionState } from 'react';
+import { useContext } from 'react';
+import { OpinionsContext } from '../store/opinions-context';
+import Submit from './Submit';
+
 export function NewOpinion() {
-  return (
-    <div id="new-opinion">
-      <h2>Share your opinion!</h2>
-      <form>
-        <div className="control-row">
-          <p className="control">
-            <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
-          </p>
+	const { addOpinion } = useContext(OpinionsContext);
 
-          <p className="control">
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
-          </p>
-        </div>
-        <p className="control">
-          <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
-        </p>
+	async function ShareOpinionAction(preState, formData) {
+		const userName = formData.get('userName');
+		const title = formData.get('title');
+		const body = formData.get('body');
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
-      </form>
-    </div>
-  );
+		const errors = [];
+
+		if (title.trim().length < 5)
+			errors.push('Title must be at least five characters long.');
+
+		if (body.trim().length < 10 || body.trim().length > 300)
+			errors.push('body must be between 10 and 300 characters long.');
+
+		if (!userName.trim()) errors.push('Please provide your name.');
+
+		if (errors.length > 0)
+			return { errors, enteredValues: { userName, title, body } };
+
+		await addOpinion({ userName, title, body });
+
+		return { errors: null };
+	}
+
+	const [formState, formAction] = useActionState(ShareOpinionAction, {
+		errors: null,
+	});
+
+	return (
+		<div id='new-opinion'>
+			<h2>Share your opinion!</h2>
+			<form action={formAction}>
+				<div className='control-row'>
+					<p className='control'>
+						<label htmlFor='userName'>Your Name</label>
+						<input
+							type='text'
+							id='userName'
+							name='userName'
+							defaultValue={formState.enteredValues?.userName}
+						/>
+					</p>
+
+					<p className='control'>
+						<label htmlFor='title'>Title</label>
+						<input
+							type='text'
+							id='title'
+							name='title'
+							defaultValue={formState.enteredValues?.title}
+						/>
+					</p>
+				</div>
+
+				<p className='control'>
+					<label htmlFor='body'>Your Opinion</label>
+					<textarea
+						id='body'
+						name='body'
+						rows={5}
+						defaultValue={formState.enteredValues?.body}
+					></textarea>
+				</p>
+
+				<ul className='errors'>
+					{formState.errors &&
+						formState.errors.map((error) => <li key={error}>{error}</li>)}
+				</ul>
+
+				<Submit />
+			</form>
+		</div>
+	);
 }
