cd7e071f613e58e896904d6ff129c611f9d4b90f
Behxrad <behxrad@yahoo.com>
2025-09-08 21:31:48 +0330

    Redux

---
+++ React/20. Redux/1. Demo/redux-demo.js
@@ -0,0 +1,24 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+	if (action.type === 'increment') {
+		return { counter: state.counter + 1 };
+	}
+
+	if (action.type === 'decrement') {
+		return { counter: state.counter - 1 };
+	}
+
+	return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const state = store.getState()
+    console.log(state)
+}
+
+store.subscribe(counterSubscriber)
+
+store.dispatch({type: 'increment'})
