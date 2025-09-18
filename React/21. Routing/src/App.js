b028c2df086cbc07f75b535efb758824647cfdfa
Behxrad <behxrad@yahoo.com>
2025-09-18 22:11:12 +0330

    Route

--- React/21. Routing/src/App.js
+++ React/21. Routing/src/App.js
@@ -1,34 +1,38 @@
 import {
 	createBrowserRouter,
-	createRoutesFromElements,
+	// createRoutesFromElements,
 	RouterProvider,
-	Route,
+	// Route,
 } from 'react-router-dom';
+
+import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
-import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 // const routeDefinitions = createRoutesFromElements(
-// 	<Route>
-// 		<Route path='/' element={<HomePage />} />
-// 		<Route path='/products' element={<ProductsPage />} />
-// 	</Route>
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
 // );
 
-const routeDefinitions = [
+const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <RootLayout />,
 		errorElement: <ErrorPage />,
 		children: [
-			{ path: '/', element: <HomePage /> },
-			{ path: '/products', element: <ProductsPage /> },
+			// { path: '', element: <HomePage /> },
+			{ index: true, element: <HomePage /> },
+			{ path: 'products', element: <ProductsPage /> },
+			{ path: 'products/:productId', element: <ProductDetailPage /> },
 		],
 	},
-];
+]);
 
-const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
 	return <RouterProvider router={router} />;
