a8ed7f8f4e3162841fda479b7d5bdb25a79e8877
Behxrad <behxrad@yahoo.com>
2025-09-26 20:50:53 +0330

    NextJS

---
+++ React/25. NextJS/1. NextJS basics/app/page.js
@@ -0,0 +1,17 @@
+import Header from '@/components/header';
+import Link from 'next/link';
+
+export default function Home() {
+	return (
+		<main>
+			<Header />
+			<p>🔥 Let&apos;s get started! 🔥</p>
+			<p>
+				<Link href='/about'>About Us</Link>
+			</p>
+			<p>
+				<Link href='/blog'>Blog</Link>
+			</p>
+		</main>
+	);
+}
