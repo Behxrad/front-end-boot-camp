2de029cd4478af51698162255ba46ff78898fe8f
Behxrad <behxrad@yahoo.com>
2025-10-09 21:28:07 +0330

    nextJS updated

---
+++ React/25. NextJS/4. Pages Router Advanced/pages/[meetupId]/index.js
@@ -0,0 +1,46 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails(props) {
+	return (
+		<MeetupDetail
+			image={props.image}
+			title={props.title}
+			address={props.address}
+			description={props.description}
+		/>
+	);
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: true,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1',
+				},
+			},
+			{
+				params: {
+					meetupId: 'm2',
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	const meetupId = context.params.meetupId;
+
+	return {
+		props: {
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+			title: 'First Meetup',
+			address: 'Some Street 5, Some City',
+			description: 'This is a first meetup',
+		},
+	};
+}
+
+export default MeetupDetails;
