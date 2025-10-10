7fa37f98093dfe1a6b217b7afed837ae8c6c01b9
Behxrad <behxrad@yahoo.com>
2025-10-10 18:57:35 +0330

    nextJS updated

--- React/25. NextJS/4. Pages Router Advanced/pages/index.js
+++ React/25. NextJS/4. Pages Router Advanced/pages/index.js
@@ -1,5 +1,7 @@
 import MeetupList from '../components/meetups/MeetupList';
 
+import { MongoClient } from 'mongodb';
+
 const DUMMY_MEETUPS = [
 	{
 		id: 'm1',
@@ -35,9 +37,25 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+	const client = await MongoClient.connect(
+		'mongodb://root:example@localhost:27017/?authSource=admin'
+	);
+
+	const db = client.db();
+	const meetupsCollection = db.collection('meetups');
+
+	const meetups = await meetupsCollection.find().toArray();
+
+	client.close();
+
 	return {
 		props: {
-			meetups: DUMMY_MEETUPS,
+			meetups: meetups.map((meetup) => ({
+				title: meetup.title,
+				address: meetup.address,
+				image: meetup.image,
+				id: meetup._id.toString(),
+			})),
 		},
 		revalidate: 1,
 	};
