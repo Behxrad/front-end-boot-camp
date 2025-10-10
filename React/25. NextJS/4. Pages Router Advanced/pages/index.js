bfde0b154266e84ddce484a917a4fd1291061867
Behxrad <behxrad@yahoo.com>
2025-10-10 19:48:16 +0330

    nextJS updated

--- React/25. NextJS/4. Pages Router Advanced/pages/index.js
+++ React/25. NextJS/4. Pages Router Advanced/pages/index.js
@@ -1,28 +1,37 @@
 import MeetupList from '../components/meetups/MeetupList';
 
 import { MongoClient } from 'mongodb';
+import Head from 'next/head';
 
-const DUMMY_MEETUPS = [
-	{
-		id: 'm1',
-		title: 'A First Meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 5, 12345 Some City',
-		description: 'This is a first meetup!',
-	},
-	{
-		id: 'm2',
-		title: 'A Second Meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 10, 12345 Some City',
-		description: 'This is a second meetup!',
-	},
-];
+// const DUMMY_MEETUPS = [
+// 	{
+// 		id: 'm1',
+// 		title: 'A First Meetup',
+// 		image:
+// 			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+// 		address: 'Some address 5, 12345 Some City',
+// 		description: 'This is a first meetup!',
+// 	},
+// 	{
+// 		id: 'm2',
+// 		title: 'A Second Meetup',
+// 		image:
+// 			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+// 		address: 'Some address 10, 12345 Some City',
+// 		description: 'This is a second meetup!',
+// 	},
+// ];
 
 function HomePage(props) {
-	return <MeetupList meetups={props.meetups} />;
+	return (
+		<>
+			<Head>
+				<title>React Meetups</title>
+				<meta name='description' content='Browse a huge list of highly active meetups!'/>
+			</Head>
+			<MeetupList meetups={props.meetups} />
+		</>
+	);
 }
 
 // export async function getServerSideProps(context) {
