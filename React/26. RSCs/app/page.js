912780b1d2d2b3b24ff6520aaab3f9c37ce61f08
Behxrad <behxrad@yahoo.com>
2025-10-12 23:11:47 +0330

    RCSs

---
+++ React/26. RSCs/app/page.js
@@ -0,0 +1,34 @@
+import ClientDemo from '@/components/ClientDemo';
+import DataFetchingDemo from '@/components/DataFetchingDemo';
+import RSCDemo from '@/components/RSCDemo';
+import ServerActionsDemo from '@/components/ServerActionsDemo';
+import UsePromiseDemo from '@/components/UsePromisesDemo';
+import { Suspense } from 'react';
+import fs from 'node:fs/promises';
+import ErrorBoundary from '@/components/ErrorBoundary';
+
+export default function Home() {
+	const futureUserPromise = new Promise((resolve, reject) =>
+		setTimeout(async () => {
+			const data = await fs.readFile('dummy-db.json', 'utf-8');
+			const users = JSON.parse(data);
+			// resolve(users);
+			reject(new Error('Error!'));
+		}, 2000)
+	);
+
+	return (
+		<main>
+			{/* <ClientDemo>
+				<RSCDemo />{' '}
+			</ClientDemo>
+			<DataFetchingDemo />
+			<ServerActionsDemo /> */}
+			<ErrorBoundary fallback={<p>Something went wrong!</p>}>
+				<Suspense fallback={<p>Loading users ...</p>}>
+					<UsePromiseDemo usersPromise={futureUserPromise} />
+				</Suspense>
+			</ErrorBoundary>
+		</main>
+	);
+}
