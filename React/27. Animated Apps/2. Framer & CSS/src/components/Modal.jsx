cca8b6df521d873be533d33fe0e9034fb54ed4f7
Behxrad <behxrad@yahoo.com>
2025-10-13 22:37:53 +0330

    Animated

--- React/27. Animated Apps/2. Framer & CSS/src/components/Modal.jsx
+++ React/27. Animated Apps/2. Framer & CSS/src/components/Modal.jsx
@@ -1,14 +1,21 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
 
 export default function Modal({ title, children, onClose }) {
-  return createPortal(
-    <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
-        <h2>{title}</h2>
-        {children}
-      </dialog>
-    </>,
-    document.getElementById('modal')
-  );
+	return createPortal(
+		<>
+			<div className='backdrop' onClick={onClose} />
+			<motion.dialog
+				open
+				className='modal'
+				initial={{ y: 30, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				exit={{ y: 30, opacity: 0 }}
+			>
+				<h2>{title}</h2>
+				{children}
+			</motion.dialog>
+		</>,
+		document.getElementById('modal')
+	);
 }
