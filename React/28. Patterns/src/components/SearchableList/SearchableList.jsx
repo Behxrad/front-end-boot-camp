2f1897eb3b7d9b773db1433744ecf05f933bc421
Behxrad <behxrad@yahoo.com>
2025-10-15 16:07:45 +0330

    Patterns

--- React/28. Patterns/src/components/SearchableList/SearchableList.jsx
+++ React/28. Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 
-export default function SearchableList({ items }) {
-  const [searchTerm, setSearchTerm] = useState('');
+export default function SearchableList({ items, children }) {
+	const [searchTerm, setSearchTerm] = useState('');
 
-  const searchResults = items.filter((item) =>
-    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
-  );
+	const searchResults = items.filter((item) =>
+		JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+	);
 
-  function handleChange(event) {
-    setSearchTerm(event.target.value);
-  }
+	function handleChange(event) {
+		setSearchTerm(event.target.value);
+	}
 
-  return (
-    <div className="searchable-list">
-      <input type="search" placeholder="Search" onChange={handleChange} />
-      <ul>
-        {searchResults.map((item, index) => (
-          <li key={index}>{item.toString()}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='searchable-list'>
+			<input type='search' placeholder='Search' onChange={handleChange} />
+			<ul>
+				{searchResults.map((item, index) => (
+					<li key={index}>{children(item)}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
