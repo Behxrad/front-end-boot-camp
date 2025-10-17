f408400da95d425157eae71a434400b5f139c1a4
Behxrad <behxrad@yahoo.com>
2025-10-17 19:57:37 +0330

    Testing

--- React/30. Testing/src/components/Greeting.test.js
+++ React/30. Testing/src/components/Greeting.test.js
@@ -1,16 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
+describe('Greeting component', () => {
+	test('renders Hello World as a text', () => {
+		//Arrange
+		render(<Greeting />);
 
+		//Act
+		//...nothing
 
-test('renders Hello World as a text', () => {
-	//Arrange
-	render(<Greeting />);
+		//Assert
+		const helloWorldElement = screen.getByText(/hello world/i);
+		expect(helloWorldElement).toBeInTheDocument();
+	});
 
-	//Act
-	//...nothing
+	test('renders It\'s good to see you as a text', () => {
+		//Arrange
+		render(<Greeting />);
 
-	//Assert
-	const helloWorldElement = screen.getByText(/hello world/i);
-    expect(helloWorldElement).toBeInTheDocument();
+		//Act
+		//...nothing
+
+		//Assert
+		const helloWorldElement = screen.getByText(/It's good to see you/i);
+		expect(helloWorldElement).toBeInTheDocument();
+	});
 });
